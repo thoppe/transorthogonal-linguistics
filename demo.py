cc8dd6491b2d46460382240f97a21bccad7ce6d4
thoppe <travis.hoppe@gmail.com>
2015-09-08 11:16:04 -0400

    Using a proper REST API endpoint now.

--- demo.py
+++ demo.py
@@ -14,6 +14,8 @@ app.logger.setLevel(logging.INFO)
 # Log the queries separately
 F_QUERY_LOG = open("log_word_query.txt",'a')
 
+default_w1, default_w2 = "boy","man"
+
 _suggest_words = ["boy","man"]
 _last_used_words = _suggest_words
 
@@ -51,71 +53,98 @@ class WordInputForm(Form):
     word1 = TextField('word1', [word_in_featureset,])
     word2 = TextField('word2', [word_in_featureset,])
 
-
-def parse_front_page(result_method):
-    global _last_used_words
+def compute_results(w1,w2,result_method):
     
-    form = WordInputForm(request.form)
-
-    if request.method == 'POST' and form.validate():
-        req = dict(request.form)
-        w1 = req["word1"][0].strip()
-        w2 = req["word2"][0].strip()
-        _last_used_words = (w1,w2)
-    else:
-        w1, w2 = _last_used_words
-
-    if not w1 or not w2:
-        w1, w2 = _suggest_words
-
     word_cutoff = 35
 
-    if form.validate() or [w1,w2]==_suggest_words:
-        result = result_method(w1, w2,
-                               features,
-                               word_cutoff=word_cutoff)
-        
-        words, distance, time = result
+    result = result_method(w1, w2, features,
+                           word_cutoff=word_cutoff)
 
-        word_blocks = bin_data(words, time)
-        result = [', '.join(x) if x else "&nbsp;"
+    words, distance, time = result
+    word_blocks = bin_data(words, time)
+    result = [', '.join(x) if x else "&nbsp;"
                   for x in word_blocks]
-
-    else:
-        result = []
 
     args = {}
     args["word1"] = w1
     args["word2"] = w2
+    args["result_list"] = result
 
+    return args
+
+
+def log_query(w1,w2):
     log_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     log_text = "QUERY {} {} {}".format(log_time, w1, w2)
     logging.info(log_text)
     F_QUERY_LOG.write(log_text+'\n')
     F_QUERY_LOG.flush()
 
-    args["result_list"] = result
-    args["form"] = form
-
-    return args
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET','POST'])
 def front_page():
-    args = parse_front_page(tol.transorthogonal_words)
-    args["starlink"] = "slerp"
+    url = '/TOL/{}/{}'.format(default_w1,default_w2)
+    return flask.redirect(url, code=302)
+
+@app.route('/TOL/', methods=['GET','POST'])
+def TOL_front_page():
+    url = '/TOL/{}/{}'.format(default_w1,default_w2)
+    return flask.redirect(url, code=302)
+
+@app.route('/TOL_SLERP/', methods=['GET','POST'])
+def TOL_SLERP_front_page():
+    url = '/TOL_SLERP/{}/{}'.format(default_w1,default_w2)
+    return flask.redirect(url, code=302)
+
+@app.route('/TOL/<w1>/<w2>', methods=['GET','POST'])
+def TOL_results(w1,w2):
+
+    form = WordInputForm(request.form)
+    result_method = tol.transorthogonal_words
+
+    if request.method == 'POST' and form.validate():
+        req = dict(request.form)
+        w1 = req["word1"][0].strip()
+        w2 = req["word2"][0].strip()
+
+        url = '/TOL/{}/{}'.format(w1,w2)
+        return flask.redirect(url, code=302)
+
+    args = compute_results(w1,w2,result_method)
+    args["form"] = form
+    args["starlink"] = "TOL_SLERP"
     args["starcolor"] = "inherit"
     args["buttontext"] = "transorthogonal path"
+
+    # Log the results!
+    log_query(w1,w2)
+    
     return flask.render_template('front.html', **args)
 
-@app.route('/slerp', methods=['GET', 'POST'])
-def slerp_page():
-    args = parse_front_page(tol.slerp_word_path)
-    args["starlink"] = ""
+@app.route('/TOL_SLERP/<w1>/<w2>', methods=['GET','POST'])
+def TOL_SLERP_results(w1,w2):
+
+    form = WordInputForm(request.form)
+    result_method = tol.slerp_word_path
+
+    if request.method == 'POST' and form.validate():
+        req = dict(request.form)
+        w1 = req["word1"][0].strip()
+        w2 = req["word2"][0].strip()
+
+        url = '/TOL_SLERP/{}/{}'.format(w1,w2)
+        return flask.redirect(url, code=302)
+
+    args = compute_results(w1,w2,result_method)
+    args["form"] = form
+
+    args["starlink"] = "TOL"
     args["starcolor"] = "blue"
     args["buttontext"] = "SLERP path"
-    return flask.render_template('front.html', **args)
 
+    # Log the results!
+    log_query(w1,w2)
+    
+    return flask.render_template('front.html', **args)
 
 
 # Load the feature set
@@ -124,4 +153,5 @@ features = tol.Features()
 
 if __name__ == '__main__':
     app.debug = False
+    #app.debug = True
     app.run()
